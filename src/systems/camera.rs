2e2beabdb26193d009a0a8161321ddc39f4db034
ghosthi <c-forsyth02@sky.com>
2026-05-29 19:52:43 +0100

    fixed camera proiblem

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -4,7 +4,9 @@ use bevy::input::mouse::{AccumulatedMouseMotion, AccumulatedMouseScroll};
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
+
         app.add_systems(Update, (zoom_camera, pan_camera));
+        app.add_systems(Startup, setup);
     }
 }
 
